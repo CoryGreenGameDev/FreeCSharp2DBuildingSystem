8bf5a25eb87b3a65829ab3b57d6ff7396dae9997
Cory Green <technerdytv@gmail.com>
2018-11-24 20:43:35 -0500

    Create Manager.cs

---
+++ Manager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+	public int Gold = 0;
+	public int Wood = 0;
+	public int Stone = 0;
+	public GameObject BuildPanel;
+	public GameObject ProducePanel;
+	public GameObject MarketPanel;
+
+	void Update () {
+		 if (Input.GetKeyDown("b")) {
+		 	if(!BuildPanel.activeSelf)
+		 	BuildPanel.SetActive(true);
+   	     }
+	}
+}
